a702840cf4587483f81ad272898e51282d2d4968
Alex Mustin <alex@alexmustin.com>
2020-01-23 17:11:10 -0800

    Fixes #10. Removes unnecessary 404 page template. Updates search form styles.

--- searchform.php
+++ searchform.php
@@ -11,14 +11,14 @@
  */
 
 $unique_id = esc_attr( uniqid( 'search-form-' ) );
-
 ?>
 
 <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<label for="<?php esc_html( $unique_id ); ?>">
 		<span class="screen-reader-text"><?php esc_html( _x( 'Search for:', 'label', 'coursemaker' ) ); ?></span>
 	</label>
-	<input type="search" id="<?php esc_html( $unique_id ); ?>" class="search-field" placeholder="<?php esc_html( esc_attr_x( 'Search &hellip;', 'placeholder', 'coursemaker' ) ); ?>" value="<?php esc_html( get_search_query() ); ?>" name="s" />
+	<input type="search" id="<?php esc_html( $unique_id ); ?>" class="search-field" placeholder="<?php echo esc_html( esc_attr_x( 'Search this website &hellip;', 'placeholder', 'coursemaker' ) ); ?>" value="<?php esc_html( get_search_query() ); ?>" name="s" />
 
-	<button type="submit" class="search-submit"><?php esc_html( course_maker_get_svg( array( 'icon' => 'search' ) ) ); ?><span class="screen-reader-text"><?php esc_html( _x( 'Search', 'submit button', 'coursemaker' ) ); ?></span></button>
+	<!-- <button type="submit" class="search-submit"><?php course_maker_get_svg( array( 'icon' => 'search' ) ); ?><span class="screen-reader-text"><?php _x( 'Search', 'submit button', 'coursemaker' ); ?></span></button> -->
+	<input class="search-submit" type="submit" value="Search">
 </form>
