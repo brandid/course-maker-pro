f440260f1bede2f6829ac262713c0f116fc2d2fe
Alex Mustin <alex@alexmustin.com>
2020-01-23 16:36:54 -0800

    Fixes #9. Adds conditional check for 'course' post type before outputting '$total_lessons' variable

--- lifterlms/course/lesson-preview.php
+++ lifterlms/course/lesson-preview.php
@@ -18,28 +18,35 @@ if ( ! $force_llms_default_styles ) {
 	?>
 
 	<div class="llms-lesson-preview<?php echo $lesson->get_preview_classes(); // phpcs:ignore ?>">
+
 		<div class="stripe"></div>
+
 		<div class="wrapper">
+
 			<a class="llms-lesson-link<?php echo $restrictions['is_restricted'] ? ' llms-lesson-link-locked' : ''; ?>" href="<?php echo ( ! $restrictions['is_restricted'] ) ? get_permalink( $lesson->get( 'id' ) ) : '#llms-lesson-locked'; ?>"<?php echo $data_msg; // phpcs:ignore ?>>
-				<?php if ( 'course' === get_post_type( get_the_ID() ) ) : ?>
-					<?php if ( apply_filters( 'llms_display_outline_thumbnails', true ) ) : ?>
-						<?php if ( has_post_thumbnail( $lesson->get( 'id' ) ) ) : ?>
+
+				<?php if ( 'course' === get_post_type( get_the_ID() ) ) { ?>
+					<?php if ( apply_filters( 'llms_display_outline_thumbnails', true ) ) { ?>
+						<?php if ( has_post_thumbnail( $lesson->get( 'id' ) ) ) { ?>
 							<div class="llms-lesson-thumbnail">
 								<?php echo get_the_post_thumbnail( $lesson->get( 'id' ) ); ?>
 							</div>
-						<?php endif; ?>
-					<?php endif; ?>
-				<?php endif; ?>
+						<?php } ?>
+					<?php } ?>
+				<?php } ?>
+
 				<section class="llms-main">
-					<?php if ( 'lesson' === get_post_type( get_the_ID() ) ) : ?>
+					<?php if ( 'lesson' === get_post_type( get_the_ID() ) ) { ?>
 						<h6 class="llms-pre-text"><?php echo $pre_text; // phpcs:ignore ?></h6>
-					<?php endif; ?>
+					<?php } ?>
 					<h5 class="llms-h5 llms-lesson-title"><?php echo esc_html( get_the_title( $lesson->get( 'id' ) ) ); ?></h5>
-					<?php if ( apply_filters( 'llms_show_preview_excerpt', true ) && llms_get_excerpt( $lesson->get( 'id' ) ) ) : ?>
+					<?php if ( apply_filters( 'llms_show_preview_excerpt', true ) && llms_get_excerpt( $lesson->get( 'id' ) ) ) { ?>
 						<div class="llms-lesson-excerpt"><?php echo llms_get_excerpt( $lesson->get( 'id' ) ); // phpcs:ignore ?></div>
-					<?php endif; ?>
+					<?php } ?>
 				</section>
+
 				<section class="llms-extra">
+
 					<p class="learn-more">
 						<?php
 						$student = llms_get_student( get_current_user_id() );
@@ -52,16 +59,23 @@ if ( ! $force_llms_default_styles ) {
 						}
 						?>
 					</p>
+
+					<?php if ( 'course' === get_post_type( get_the_ID() ) ) { ?>
 					<p class="extras">
 						<span class="llms-lesson-counter"><?php printf( _x( '%1$d of %2$d', 'lesson order within section', 'lifterlms' ), $lesson->get_order(), $total_lessons ); // phpcs:ignore ?></span>
 						<?php echo $lesson->get_preview_icon_html(); // phpcs:ignore ?>
 					</p>
+					<?php } ?>
+
 				</section>
 				<div class="clear"></div>
-				<?php if ( $restrictions['is_restricted'] ) : ?>
-				<?php endif; ?>
+
+				<?php if ( $restrictions['is_restricted'] ) { ?>
+				<?php } ?>
 			</a>
+
 		</div>
+
 	</div>
 
 <?php
