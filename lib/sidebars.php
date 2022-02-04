7c5cdc8790fa0b9c7b19cb1003342b1c47a4c67f
Ronald Huereca <ronalfy@gmail.com>
2022-02-04 10:08:26 -0600

    Adding paragraph styles to about page.

--- lib/sidebars.php
+++ lib/sidebars.php
@@ -16,7 +16,7 @@ function course_maker_register_sidebars() {
 			'id'            => 'about-sidebar',
 			'name'          => __( 'About Sidebar', 'coursemaker' ),
 			'description'   => __( 'Sidebar for the About page.', 'coursemaker' ),
-			'before_widget' => '<div id="%1$s" class="widget widget-about %2$s"><div class="widget-wrap">',
+			'before_widget' => '<div id="%1$s" class="widget widget-general widget-about %2$s"><div class="widget-wrap">',
 			'after_widget'  => '</div></div>',
 			'before_title'  => '<h4 class="widgettitle about-widget-title">',
 			'after_title'   => '</h4>',
