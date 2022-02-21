957aeaf19845e6e7ed1fbd77629dd9d86e1cd801
Ronald Huereca <ronalfy@gmail.com>
2022-02-21 15:27:41 -0600

    Beginning translating to jQuery plugin.

--- js/featured-article-image.js
+++ js/featured-article-image.js
@@ -173,6 +173,131 @@
 		mediaUploader.open();
 	};
 
+	$.fn.courseMakerImageCrop = function( options ) {
+
+		var __ = wp.i18n.__;
+
+		var settings = $.extend( {
+			attachmentId: 0,
+			aspectRatio: '1:1',
+			suggestedWidth: '1000',
+			suggestedHeight: '1000',
+			nonce: '',
+			postId: 0,
+			title: __( 'Image', 'course-maker' ),
+			buttonLabel: __( 'Add Image', 'course-maker' ),
+		}, options );
+
+		var cropOptions = function (attachment, controller) {
+			var control = controller.get('control');
+			var realWidth = attachment.get('width');
+			var realHeight = attachment.get('height');
+	
+			var xInit = parseInt(control.params.width, 10);
+			var yInit = parseInt(control.params.height, 10);
+	
+			var ratio = xInit / yInit;
+			var ratioReal = realWidth / realHeight;
+	
+			// Determine if user can skip crop.
+			var canSkipCrop = false;
+	
+			// If ratios match, can skip crop.
+			if (ratio === ratioReal) {
+				canSkipCrop = true;
+			}
+	
+			// If image is smaller than cropped.
+			if (realWidth < control.params.width && realHeight < control.params.height) {
+				canSkipCrop = true;
+			}
+	
+			controller.set('canSkipCrop', canSkipCrop);
+	
+			var xImg = xInit;
+			var yImg = yInit;
+	
+			if (realWidth / realHeight > ratio) {
+				if (yImg > realHeight) {
+					yImg = realHeight;
+				}
+				yInit = yImg;
+				xInit = yInit * ratio;
+			} else {
+				if (xImg > realWidth) {
+					xImg = realWidth;
+				}
+				xInit = xImg;
+				yInit = xInit / ratio;
+			}
+	
+			var x1 = (realWidth - xInit) / 2;
+			var y1 = (realHeight - yInit) / 2;
+	
+			var imgSelectOptions = {
+				handles: true,
+				keys: true,
+				instance: true,
+				persistent: true,
+				imageWidth: realWidth,
+				imageHeight: realHeight,
+				x1,
+				y1,
+				x2: xInit + x1,
+				y2: yInit + y1,
+				aspectRatio: settings.aspectRatio,
+			};
+			return imgSelectOptions;
+		};
+
+		var cropControl = {
+			id: 'control-id',
+			params: {
+				flex_width: true, // set to true if the width of the cropped image can be different to the width defined here
+				flex_height: true, // set to true if the height of the cropped image can be different to the height defined here
+				width: settings.suggestedWidth, // set the desired width of the destination image here
+				height: settings.suggestedHeight, // set the desired height of the destination image here
+			},
+		};
+
+		var mediaUploader = wp.media({
+			button: {
+				text: settings.buttonLabel,
+				close: true,
+			},
+			states: [
+				new wp.media.controller.Library({
+					title: settings.title,
+					library: wp.media.query({ type: 'image' }),
+					multiple: false,
+					date: false,
+					priority: 20,
+					suggestedWidth: settings.suggestedWidth,
+					suggestedHeight: settings.suggestedHeight,
+				}),
+				new wp.media.controller.CustomizeImageCropper({
+					imgSelectOptions: cropOptions,
+					control: cropControl,
+				}),
+			],
+		});
+
+		return this.each( function() {
+			console.log( settings );
+		} );
+	}
+
+	jQuery( '#course-maker-featured-article-image' ).courseMakerImageCrop( {
+		attachmentId: course_maker_meta_attachments.attachment_id,
+			aspectRatio: course_maker_meta_attachments.aspect_ratio,
+			suggestedWidth: course_maker_meta_attachments.suggested_width,
+			suggestedHeight: course_maker_meta_attachments.suggested_height,
+			nonce: course_maker_meta_attachments.nonce,
+			postId: course_maker_meta_attachments.post_id,
+			title: course_maker_meta_attachments.media_modal_title,
+			buttonLabel: course_maker_meta_attachments.media_modal_crop_text,
+	});
+
 	jQuery('#course-maker-featured-article-image, .course-maker-img-container a').on('click', handleUpload);
 
 })(jQuery);
