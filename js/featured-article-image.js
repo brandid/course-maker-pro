039ea47ae1f8c6cd0aa5af3e0c2556e6f201e055
Ronald Huereca <ronalfy@gmail.com>
2022-02-21 13:09:15 -0600

    JS formatting.

--- js/featured-article-image.js
+++ js/featured-article-image.js
@@ -1,18 +1,18 @@
 // any global scripts needed for theme goes here
 ;
-(function($) {
+(function ($) {
 
-    'use strict';
+	'use strict';
 	var __ = wp.i18n.__;
 	var saved_attachment_id = course_maker_meta_attachments.attachment_id;
 
-	var cropOptions = ( attachment, controller ) => {
-		var control = controller.get( 'control' );
-		var realWidth = attachment.get( 'width' );
-		var realHeight = attachment.get( 'height' );
+	var cropOptions = function (attachment, controller) {
+		var control = controller.get('control');
+		var realWidth = attachment.get('width');
+		var realHeight = attachment.get('height');
 
-		let xInit = parseInt( control.params.width, 10 );
-		let yInit = parseInt( control.params.height, 10 );
+		var xInit = parseInt(control.params.width, 10);
+		var yInit = parseInt(control.params.height, 10);
 
 		var ratio = xInit / yInit;
 		var ratioReal = realWidth / realHeight;
@@ -21,36 +21,36 @@
 		var canSkipCrop = false;
 
 		// If ratios match, can skip crop.
-		if ( ratio === ratioReal ) {
+		if (ratio === ratioReal) {
 			canSkipCrop = true;
 		}
 
 		// If image is smaller than cropped.
-		if ( realWidth < control.params.width && realHeight < control.params.height ) {
+		if (realWidth < control.params.width && realHeight < control.params.height) {
 			canSkipCrop = true;
 		}
-		
-		controller.set( 'canSkipCrop', canSkipCrop );
+
+		controller.set('canSkipCrop', canSkipCrop);
 
 		var xImg = xInit;
 		var yImg = yInit;
 
-		if ( realWidth / realHeight > ratio ) {
-			if ( yImg > realHeight ) {
+		if (realWidth / realHeight > ratio) {
+			if (yImg > realHeight) {
 				yImg = realHeight;
 			}
 			yInit = yImg;
 			xInit = yInit * ratio;
 		} else {
-			if ( xImg > realWidth ) {
+			if (xImg > realWidth) {
 				xImg = realWidth;
 			}
 			xInit = xImg;
 			yInit = xInit / ratio;
 		}
 
-		var x1 = ( realWidth - xInit ) / 2;
-		var y1 = ( realHeight - yInit ) / 2;
+		var x1 = (realWidth - xInit) / 2;
+		var y1 = (realHeight - yInit) / 2;
 
 		var imgSelectOptions = {
 			handles: true,
@@ -72,10 +72,10 @@
 	 * Set the attachment image.
 	 * @param {object} image Image object after cropping.
 	 */
-	function setAttachmentImage( image ) {
+	function setAttachmentImage(image) {
 		var html = '<img src="' + image.url + '" />';
 
-		$( '.course-maker-img-container a' ).html( html );
+		$('.course-maker-img-container a').html(html);
 	}
 
 	var cropControl = {
@@ -89,8 +89,8 @@
 	};
 
 	// Ajax to save cropped image.
-	function saveCropped( attachment_id, attachment_url ) {
-		$.ajax( {
+	function saveCropped(attachment_id, attachment_url) {
+		$.ajax({
 			type: 'POST',
 			url: ajaxurl,
 			data: {
@@ -100,57 +100,55 @@
 				url: attachment_url,
 				post_id: course_maker_meta_attachments.post_id,
 			},
-		} ).done( function( response ) {
-			if ( response.success ) {
-				setAttachmentImage( {
+		}).done(function (response) {
+			if (response.success) {
+				setAttachmentImage({
 					id: response.data.attachment_id,
 					url: response.data.img_url,
-				} );
+				});
 				saved_attachment_id = response.data.attachment_id;
 			}
-		}).fail( function( response ) {
-			console.log( 'failed' );
-		}).always( function( response ) {
-			console.log( 'finished' );
+		}).fail(function (response) {
+		}).always(function (response) {
 		});
 	};
 
-	function handleUpload( e ) {
+	function handleUpload(e) {
 		e.preventDefault();
-		var mediaUploader = wp.media( {
+		var mediaUploader = wp.media({
 			button: {
 				text: course_maker_meta_attachments.media_modal_crop_text,
 				close: false,
 			},
 			states: [
-				new wp.media.controller.Library( {
+				new wp.media.controller.Library({
 					title: course_maker_meta_attachments.media_modal_title,
-					library: wp.media.query( { type: 'image' } ),
+					library: wp.media.query({ type: 'image' }),
 					multiple: false,
 					date: false,
 					priority: 20,
 					suggestedWidth: course_maker_meta_attachments.suggested_width,
 					suggestedHeight: course_maker_meta_attachments.suggested_height,
-				} ),
-				new wp.media.controller.CustomizeImageCropper( {
+				}),
+				new wp.media.controller.CustomizeImageCropper({
 					imgSelectOptions: cropOptions,
 					control: cropControl,
-				} ),
+				}),
 			],
-		} );
+		});
 
 		// When image is cropped.
-		mediaUploader.on( 'cropped', function( croppedImage ) {
-			saveCropped( croppedImage.attachment_id, croppedImage.url );
-		} );
+		mediaUploader.on('cropped', function (croppedImage) {
+			saveCropped(croppedImage.attachment_id, croppedImage.url);
+		});
 
 		// When image cropping is skipped.
-		mediaUploader.on( 'skippedcrop', function( selection ) {
-			saveCropped( selection.id, selection.get( 'url' ) );
-		} );
+		mediaUploader.on('skippedcrop', function (selection) {
+			saveCropped(selection.id, selection.get('url'));
+		});
 
-		mediaUploader.on( 'select', function() {
-			var attachment = mediaUploader.state().get( 'selection' ).first().toJSON();
+		mediaUploader.on('select', function () {
+			var attachment = mediaUploader.state().get('selection').first().toJSON();
 
 			var ratio = attachment.width / attachment.height;
 			var desiredRatio = cropControl.params.width / cropControl.params.height;
@@ -158,23 +156,23 @@
 				ratio === desiredRatio
 			) {
 				var selection = mediaUploader.state().get('selection').single();
-				saveCropped( selection.attributes.id, selection.attributes.url );
+				saveCropped(selection.attributes.id, selection.attributes.url);
 				mediaUploader.close();
 			} else {
-				mediaUploader.setState( 'cropper' );
+				mediaUploader.setState('cropper');
 			}
-		} );
+		});
 
-		mediaUploader.on( 'open', function() {
+		mediaUploader.on('open', function () {
 			// Set attached image upon media library opened.
-			var attachment = wp.media.attachment( saved_attachment_id );
+			var attachment = wp.media.attachment(saved_attachment_id);
 			var selection = mediaUploader.state('library').get('selection');
-			selection.add( attachment );
-		} );
+			selection.add(attachment);
+		});
 
 		mediaUploader.open();
 	};
 
-	jQuery( '#course-maker-featured-article-image, .course-maker-img-container a' ).on( 'click', handleUpload );
+	jQuery('#course-maker-featured-article-image, .course-maker-img-container a').on('click', handleUpload);
 
 })(jQuery);
